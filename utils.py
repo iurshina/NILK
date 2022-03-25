4b26940f7cc569a812adcc5e60107dcae5786e48
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-03-25 12:24:01 +0100

    Adds some utils

---
+++ utils.py
@@ -0,0 +1,18 @@
+import os
+import gzip
+import json
+
+DIR = "../chengxi/data_process/entities_json_data/"
+
+
+def get_entities_with_wikilink():
+    for file in os.listdir(DIR):
+        with gzip.open(DIR + file, 'r') as f, open("entites_with_links.tsv", 'w') as el:
+            for line in f:
+                line = line[:-2]  # remove comma from the end of the line (,\n)
+                j_content = json.loads(line)
+                if j_content["wiki_sitelink"] is not None:
+                    el.write(j_content["id"] + "\t" + j_content["wiki_sitelink"] + "\n")
+
+
+get_entities_with_wikilink()
