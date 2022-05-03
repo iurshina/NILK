a63dffe8dbc5cd47bfc9f4dc62f290d22336b551
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-05-03 14:43:03 +0200

    NIls stat script

--- get_nil_stats.py
+++ get_nil_stats.py
@@ -7,8 +7,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-n', '--nils_file', default="nil_mentions.json")
+    parser.add_argument('-m', '--mapping_file', default="wiki_wikidata_mapping_2021.json")
 
     args = parser.parse_args()
+
+    mapping = {}
+    with open(args.mapping_file) as f:
+        for l in f:
+            line = json.loads(l)
+            id = line["id"]
+            enwiki = line["enwiki"]
+
+            mapping[id] = enwiki
 
     c = Counter()
 
@@ -18,7 +28,7 @@ if __name__ == '__main__':
 
             wikidata_id = line["wikidata_id"]
 
-            c[wikidata_id] += 1
+            c[mapping[wikidata_id]] += 1
 
     print("Number of nil entities: " + str(len(c.keys())))
 
