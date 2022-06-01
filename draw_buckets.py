c1004e43bcbfe82608f97d5dafa1881e1f761018
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-06-01 18:28:17 +0200

    count

--- draw_buckets.py
+++ draw_buckets.py
@@ -6,10 +6,13 @@ import matplotlib.pylab as plt
 import numpy as np
 import math
 
-c = Counter()
+# c = Counter()
+# c_nil = Counter
 set_c_nil = set()
 set_c_not_nil = set()
-with open("all_mentions.json") as f:
+m_n = 0
+m_not_n = 0
+with open("all_mention_01_06.json") as f:
     for l in f:
         line = json.loads(l)
         wikidata_id = line["wikidata_id"]
@@ -17,14 +20,20 @@ with open("all_mentions.json") as f:
 
         if is_nil:
             # print(str(is_nil))
+            # c_nil[wikidata_id] += 1
             set_c_nil.add(wikidata_id)
+            m_n += 1
         else:
             set_c_not_nil.add(wikidata_id)
-            c[wikidata_id] += 1
+            # c[wikidata_id] += 1
+            m_not_n += 1
 
 
-print("N not nil " + str(len(set_c_not_nil)))
-print("N nil " + str(len(set_c_nil)))
+print("N entities not nil " + str(len(set_c_not_nil)))
+print("N entities nil " + str(len(set_c_nil)))
+
+print("N mentions not nil " + str(m_n))
+print("N mentions nil " + str(m_not_n))
 
 # vv = list(c.values())
 # vv = sorted(vv)
@@ -49,23 +58,23 @@ print("N nil " + str(len(set_c_nil)))
 
 # plt.hist(sorted(c.values()), 10, density=True, facecolor='g', alpha=0.75)
 #
-buckets = {}
-for id, n in c.items():
-    k = "1"
-    if 1 < n < 5:
-        k = "2 to 5"
-    if 5 <= n < 100:
-        k = "6 to 100"
-    if 100 <= n < 1000:
-        k = "101 to 1000"
-    if 1000 <= n:
-        k = "1000+"
-    if k not in buckets:
-        buckets[k] = 0
-    buckets[k] += 1
-
-print(str(buckets))
-print(str(c))
+# buckets = {}
+# for id, n in c.items():
+#     k = "1"
+#     if 1 < n < 5:
+#         k = "2 to 5"
+#     if 5 <= n < 100:
+#         k = "6 to 100"
+#     if 100 <= n < 1000:
+#         k = "101 to 1000"
+#     if 1000 <= n:
+#         k = "1000+"
+#     if k not in buckets:
+#         buckets[k] = 0
+#     buckets[k] += 1
+#
+# print(str(buckets))
+# print(str(c))
 
 #
 # lists = sorted(buckets.items())
