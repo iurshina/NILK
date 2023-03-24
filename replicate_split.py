4898d19f9942aa2b7a2cd566fac176a656619549
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2023-03-24 16:48:36 +0100

    Replicate split: bugfix

--- replicate_split.py
+++ replicate_split.py
@@ -1,12 +1,14 @@
 import json
+import tqdm
 
 
-def read_ids(file_name: str):
-    res = []
+def read_ids(file_name: str) -> set:
+    res = set()
     with open(file_name) as f:
         for l in f:
             obj = json.loads(l)
-            res.append(obj['id'])
+            res.add(obj['id'])
+    print(f'Gathered {file_name}')
     return res
 
 
@@ -14,11 +16,11 @@ train_ids, val_ids, test_ids = read_ids('train.jsonl'), read_ids('valid.jsonl'),
 
 with open('all_mentions_3_03_2023.json') as f, open('train_1000.json', 'w') as tr_f, \
      open('valid_1000.json', 'w') as va_f, open('test_1000.json', 'w') as te_f:
-    for l in f:
+    for l in tqdm.tqdm(f, total=107_681_481):
         obj = json.loads(l)
         id_ = obj['id']
         if id_ in train_ids:
-            te_f.write(l)
+            tr_f.write(l)
         elif id_ in val_ids:
             va_f.write(l)
         elif id_ in test_ids:
